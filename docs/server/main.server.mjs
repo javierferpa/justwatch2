d96fe8d435c679c7683315ca4e366167ca57f8a0
JAVIER FERNANDEZ <javierferpa@gmail.com>
2023-12-11 17:11:53 +1100

    #

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-KY24HALB.mjs";import"./chunk-6GBKHVKO.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/card-detail/card-detail.component.ts
+++ src/app/card-detail/card-detail.component.ts
@@ -13,11 +13,39 @@ export class CardDetailComponent {
   cardId: string | undefined;
 
   constructor(private route: ActivatedRoute) {}
+  movieDetails: any = {};
+  watchProviders: any[] = [];
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.cardId = params['id'];
       // Puedes cargar los detalles de la tarjeta con this.cardId
     });
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDYzNjY5ZWRhMTNiOWQ4ODA2ZDlkMGIxYTI1NWVlNyIsInN1YiI6IjY1NjEzOTI0MzY3OWExMDk3N2UwMzQxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gpPYfhFoIc9PBlcw9nP_AE6_9iv4sx27vyB_r27J07M'
+      }
+    };
+
+    console.log('detail')
+    fetch('https://api.themoviedb.org/3/movie/' + this.cardId + '?language=en-US', options)
+      .then(response => response.json())
+      .then(response => {
+        this.movieDetails = response;
+        console.log(response);
+      })
+      .catch(err => console.error(err));
+
+    console.log('where')
+    fetch('https://api.themoviedb.org/3/movie/' + this.cardId + '/watch/providers', options)
+      .then(response => response.json())
+      .then(response => {
+        this.watchProviders = response.results.AU.buy;
+        console.log(this.watchProviders);
+      })
+      .catch(err => console.error(err));
   }
 }
